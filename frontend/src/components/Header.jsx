f92dba2dfd5f0d445370843dcd50af665dcae2a9
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-11-06 13:23:05 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <div className="space-y-2 text-center">
       <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">
-        TodoX
+        Todo - Kai Nguyen
       </h1>
 
       <p className="text-muted-foreground">
